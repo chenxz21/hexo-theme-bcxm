7c1b386be81954feb5910b5b6f59cf29d04e0d5e
chenxz21 <1986994368@qq.com>
2022-02-09 12:17:15 +0800

    二改butterfly

---
+++ scripts/events/welcome.js
@@ -0,0 +1,9 @@
+const logger = require('hexo-log')()
+
+hexo.on('ready', () => {
+  const { version } = require('../../package.json')
+  logger.info(`
+  --------------------------butterfly--------------------------
+                            ${version}       
+`)                
+})
