d40c5f3a6eed1bf9aff9a3e192cf9b7371d99f09
chenxz21 <1986994368@qq.com>
2022-05-21 12:12:23 +0800

    更改配置

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -3,6 +3,7 @@ const logger = require('hexo-log')()
 hexo.on('ready', () => {
   const { version } = require('../../package.json')
   logger.info(`
-  --------------------------butterfly--------------------------     
+  --------------------------butterfly--------------------------
+                            ${version}       
 `)                
 })
